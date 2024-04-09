e9c35cf36d92b3ea55e73d0024223e30e774e988
Anthony Osigbe <thronesofglory@gmail.com>
2024-04-09 04:20:52 -0700

    0. Pascal's Triangle tasks completed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,31 +2,33 @@
 '''Module for discovering Pascal's Triangle numbers'''
 
 
+
 def pascal_triangle(n):
     '''
     Function to generate Pascal's Triangle
 
-    Parameters:
-        n (int): The number of rows of Pascal's triangle to generate.
+    Parameters: n (int): The number of rows of Pascal's
+    triangle to generate.
 
-    Returns:
-        pascal_triangle (list): A list of lists representing Pascal's Triangle.
+    Returns: A list of lists representing Pascal's Triangle.
     '''
     pascal_triangle = []
 
     if n <= 0:
         return pascal_triangle
 
-    # Generate rows.
+    '''Generate rows.'''
     for row in range(n):
-        new_row = [1]  # First element is always 1.
+    '''First element is always 1.'''
+        new_row = [1]
 
-        # Fill in the middle elements.
+    '''Fill in the middle elements.'''
         for col in range(1, row):
             new_row.append(pascal_triangle[-1][col-1] + pascal_triangle[-1][col])
 
         if row > 0:
-            new_row.append(1)  # Last element is always 1.
+            new_row.append(1)
+    '''Last element is always 1.'''
         pascal_triangle.append(new_row)
 
     return pascal_triangle
