22f06f2aec646a73adc70333d71a2d2f0e7d80c9
Anthony Osigbe <thronesofglory@gmail.com>
2024-04-09 04:35:31 -0700

    0. Pascal's Triangle correction made

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-'''Module for discovering Pascal's Triangle numbers'''
+"""
+Module for discovering Pascal's Triangle numbers
+"""
+
 
 
 def pascal_triangle(n):
-    '''
+    """
     Function to generate Pascal's Triangle
 
     Parameters: n (int): The number of rows of Pascal's
     triangle to generate.
 
     Returns: A list of lists representing Pascal's Triangle.
-    '''
+    """
     pascal_triangle = []
 
     if n <= 0:
         return pascal_triangle
-
+    
+    """First element is always 1."""
     for row in range(n):
-        '''First element is always 1.'''
         new_row = [1]
 
-        '''Fill in the middle elements.'''
+        """Fill in the middle elements."""
         for col in range(1, row):
             new_row.append(pascal_triangle[-1][col-1] + pascal_triangle[-1][col])
 
+            """Last element is always 1."""
         if row > 0:
             new_row.append(1)
-            '''Last element is always 1.'''
         pascal_triangle.append(new_row)
 
     return pascal_triangle
